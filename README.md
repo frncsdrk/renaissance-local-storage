f5cf0e9a2e03243103ad4187bdcedf76b0a30dac
frncsdrk <carlsmoh@gmail.com>
2017-02-25 20:15:20 +0100

    Add initial content

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # renaissance-local-storage
-local storage adapter for renaissance 
+local storage adapter for renaissance
+
+## usage
+- include `localStorage.js` in your paths
+- load it in your page component
+- call the include
+
+## API
+.save({string} key, {string} value)  
+.update({string} key, {string} value)
+.get({string} key)
+.remove({string} key)
+.clear()
---
+++ localStorage.js
@@ -0,0 +1,37 @@
+// local storage
+'use strict';
+
+define(
+    [
+        'lib/core'
+    ],
+    function(renaissance) {
+        function localStorageAdapter() {
+            this.save = function(key, value) {
+                localStorage.setItem(key, value);
+                return true;
+            };
+            this.update = function(key, value) {
+                if (!localStorage.getItem(key)) {
+                    return false;
+                }
+                localStorage.setItem(key, value);
+                return true;
+            };
+            this.get = function(key) {
+                return localStorage.getItem(key);
+            };
+            this.remove = function(key) {
+                localStorage.removeItem(key);
+                return true;
+            };
+            this.clear = function() {
+                localStorage.clear();
+            };
+        }
+
+        return function() {
+            renaissance.registerAdapter('localStorage', localStorageAdapter);
+        };
+    }
+);
